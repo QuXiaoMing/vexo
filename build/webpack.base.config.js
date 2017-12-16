109bc139e4718ce8b34487215a1a5cebedb24a33
quxiaoming <xm726@qq.com>
2017-12-16 20:31:14 +0800

    【修复】打包路径问题

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -16,7 +16,7 @@ module.exports = {
   entry: [resolve('/src/main.js')],
   output: {
     path: resolve('/dist'),
-    filename: 'bundle-[hash].js'
+    filename: 'js/[name].js'
   },
   resolve: {
     alias: {
@@ -78,6 +78,30 @@ module.exports = {
           // use style-loader in development 
           fallback: "style-loader"
         })
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: './img/[name].[hash:7].[ext]'
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: './media/[name].[hash:7].[ext]'
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: './fonts/[name].[hash:7].[ext]'
+        }
       }
     ]
   },
--- themes/layout/header.vue
+++ themes/layout/header.vue
@@ -1,3 +1,27 @@
 <template>
-  <h1>header</h1>
+  <header class="top-nav-container">
+  
+    <nav class="top-nav">
+  
+      <a class="vertical-center nav-logo active" href="#!/posts">
+  
+        <img src="../assets/img/logo.png" class="logo vertical-center-content" alt="">
+  
+        <span class="brand vertical-center-content">Shellming</span> </a>
+  
+      <ul class="nav-link-container">
+  
+        <li class="nav-link-item"><a class="nav-link active" href="#!/posts">文章</a></li>
+  
+        <li class="nav-link-item"><a class="nav-link" href="#!/tags">标签</a></li>
+  
+        <li class="nav-link-item"><a class="nav-link" target="_blank" href="https://chuckliu.me/leetcode/#/cover">leetcode</a></li>
+  
+        <li class="nav-link-item"><a class="nav-link" href="#!/me">关于我</a></li>
+  
+      </ul> <img src="../assets/img/menu.png"
+  
+        alt="" class="menu-button"> </nav>
+  
+  </header>
 </template>
