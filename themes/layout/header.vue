394c11abfc50121c7e81443a186fe2ef1ce708e0
quxiaoming <xm726@qq.com>
2017-12-16 21:16:50 +0800

    【增加】header

--- themes/layout/header.vue
+++ themes/layout/header.vue
@@ -1,27 +1,15 @@
 <template>
-  <header class="top-nav-container">
-  
-    <nav class="top-nav">
-  
-      <a class="vertical-center nav-logo active" href="#!/posts">
-  
-        <img src="../assets/img/logo.png" class="logo vertical-center-content" alt="">
-  
-        <span class="brand vertical-center-content">Shellming</span> </a>
-  
-      <ul class="nav-link-container">
-  
-        <li class="nav-link-item"><a class="nav-link active" href="#!/posts">文章</a></li>
-  
-        <li class="nav-link-item"><a class="nav-link" href="#!/tags">标签</a></li>
-  
-        <li class="nav-link-item"><a class="nav-link" target="_blank" href="https://chuckliu.me/leetcode/#/cover">leetcode</a></li>
-  
-        <li class="nav-link-item"><a class="nav-link" href="#!/me">关于我</a></li>
-  
-      </ul> <img src="../assets/img/menu.png"
-  
-        alt="" class="menu-button"> </nav>
-  
-  </header>
+<header class="top-nav-container">
+  <nav class="top-nav">
+    <router-link to="/" class="vertical-center nav-logo active" >
+      <img src="../assets/img/logo.png" class="logo vertical-center-content" alt="">
+      <span class="brand vertical-center-content">Shellming</span>
+    </router-link>
+    <ul class="nav-link-container">
+      <li class="nav-link-item"><router-link class="nav-link active" to="/post">文章</router-link ></li>
+      <li class="nav-link-item"><router-link class="nav-link" to="/tags">标签</router-link ></li>
+      <li class="nav-link-item"><router-link class="nav-link" to="/me">关于我</router-link ></li>
+    </ul> <img src="../assets/img/menu.png" alt="" class="menu-button">
+  </nav>
+</header>
 </template>
