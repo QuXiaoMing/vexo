193f491663da267d7333d0f97eccebdee0a78696
quxiaoming <quxiaoming@szzt.com.cn>
2018-02-24 13:34:11 +0800

    【修改】mixins

--- build/compile-md.js
+++ build/compile-md.js
@@ -3,9 +3,7 @@ module.exports = function compileMD (source) {
   return source
     .replace(/^---[\s\S]*?---/g, '\n')
     + ` <script>
-          import mixins from '@/mixins/addPostinfo.js'
           export default {
-            mixins: [mixins]
           }
         </script>`
 }
